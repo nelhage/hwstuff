d5f149c8d731c12ae5cd8726bbce15d4c29a1354
Nelson Elhage <nelhage@nelhage.com>
2023-05-19 19:00:48 -0500

    an ALU!

--- riscv/alu.sv
+++ riscv/alu.sv
@@ -1,24 +1,36 @@
 
 module alu(
-           input logic [31:0] a, b,
-           input logic [2:0] ctl,
+           input logic [31:0]  a, b,
+           input logic ctl_ex,
+           input logic [2:0]   ctl,
            output logic [31:0] result
 );
+  // always
+  //   begin
+  //     $display("ctl=%b a=%b b=%b", ctl, a, b);
+  //     $display("a<b=%b, signed[a<b]=%b", a<b, signed_a<signed_b);
+  //   end
+
   always_comb
     begin
       case (ctl)
         // ADD
-        3'b000: result = a + b;
+        3'b000: result = (ctl_ex == 0) ? a + b : a - b;
         // SLT
-        3'b010: result = {31'b0, a < b};
-        // SLTI
-        3'b011: result = 0;
+        3'b010: result = {31'b0, signed'(a) < signed'(b)};
+        // SLTU
+        3'b011: result = {31'b0, a < b};
         // XOR
         3'b100: result = a^b;
         // OR
         3'b110: result = a|b;
         // AND
         3'b111: result = a&b;
+        // SLL
+        3'b001: result = a << b[4:0];
+        // SR[AL]
+        3'b101: result = (ctl_ex == 0) ? (a >> b[4:0]) : signed'(signed'(a) >>> b[4:0]);
+
         default: result = 0;
       endcase
     end
--- riscv/alu_test.cc
+++ riscv/alu_test.cc
@@ -1,7 +1,115 @@
 #include "Valu.h"
+#include <verilated.h>
 
-int main() {
+#include <memory>
+#include <stdint.h>
+#include <stdio.h>
+
+using std::make_unique;
+
+struct AluTest {
+  uint32_t ctl_ex : 1;
+  uint32_t ctl : 3;
+  uint32_t a, b;
+
+  uint32_t expect;
+};
+
+int main(int argc, char **argv) {
+  auto contextp = make_unique<VerilatedContext>();
+  contextp->commandArgs(argc, argv);
+
   Valu alu;
 
-  return 0;
+  AluTest test_cases[] = {
+      // ADD
+      {0, 0x0, 1234, 9823, 1234 + 9823},
+
+      // SUB
+      {1, 0x0, 1234, 9823, uint32_t(1234 - 9823)},
+      {1, 0x0, 0, 1, 0xffffffff},
+      {1, 0x0, 0xffffffff, 1, 0xfffffffe},
+      {1, 0x0, 0, 0xffffffff, 1},
+
+      {0, 0x2, 0, 1, 1},
+      {0, 0x2, 1, 0, 0},
+      {0, 0x2, 0xffffffff, 0, 1},
+      {0, 0x2, 0, 0xffffffff, 0},
+
+      {0, 0x3, 0, 1, 1},
+      {0, 0x3, 1, 0, 0},
+      {0, 0x3, 0xffffffff, 0, 0},
+      {0, 0x3, 0, 0xffffffff, 1},
+
+      // XOR
+      {0, 0x4, 0, 1, 1},
+      {0, 0x4, 1, 0, 1},
+      {0, 0x4, 1112, 1112, 0},
+      {0, 0x4, 0xaaaaaaaa, 0xffffffff, 0x55555555},
+
+      // OR
+      {0, 0x6, 0x01, 0x02, 0x03},
+      {0, 0x6, 0x11111111, 0x44444444, 0x55555555},
+      {0, 0x6, 0xaaaaaaaa, 0x55555555, 0xffffffff},
+
+      // AND
+      {0, 0x7, 0x01, 0x02, 0x00},
+      {0, 0x7, 0x11111111, 0x44444444, 0},
+      {0, 0x7, 0xffffffff, 0x12345678, 0x12345678},
+      {0, 0x7, 0x12345678, 0xffffffff, 0x12345678},
+      {0, 0x7, 0x12345678, 0xf0f0f0f0, 0x10305070},
+
+      // SLL
+      {0, 0x1, 0x1, 0x1, 0x02},
+      {0, 0x1, 0xabc, 12, 0xabc000},
+      {0, 0x1, 0xabc, 24, 0xbc000000},
+      {0, 0x1, 0xffffffff, 31, 0x80000000},
+      {0, 0x1, 0xffffffff, 32, 0xffffffff},
+
+      // SRL
+      {0, 0x5, 0x02, 0x1, 0x1},
+      {0, 0x5, 0xabc000, 12, 0xabc},
+      {0, 0x5, 0xbc000000, 24, 0xbc},
+      {0, 0x5, 0x80000000, 31, 0x1},
+      {0, 0x5, 0xffffffff, 32, 0xffffffff},
+
+      // SRA
+      {1, 0x5, 0x02, 0x1, 0x1},
+      {1, 0x5, 0xabc000, 12, 0xabc},
+      {1, 0x5, 0xbc000000, 24, 0xffffffbc},
+      {1, 0x5, 0x3c000000, 24, 0x3c},
+      {1, 0x5, 0x80000000, 31, 0xffffffff},
+      {1, 0x5, 0xffffffff, 31, 0xffffffff},
+      {1, 0x5, 0xffffffff, 3, 0xffffffff},
+      {1, 0x5, 0xffffffff, 32, 0xffffffff},
+  };
+
+  int ok = 0, ntest = 0;
+
+  for (auto &tc : test_cases) {
+    ntest += 1;
+    alu.ctl_ex = tc.ctl_ex;
+    alu.ctl = tc.ctl;
+    alu.a = tc.a;
+    alu.b = tc.b;
+    alu.eval();
+
+    if (alu.result != tc.expect) {
+      fprintf(stderr, "[FAILED] ctl=%d:%x a=%08x b=%08x expect=%08x != %08x\n",
+              tc.ctl_ex, tc.ctl, tc.a, tc.b, tc.expect, alu.result);
+    } else {
+      ok += 1;
+    }
+  }
+
+  alu.final();
+
+  if (ok == ntest) {
+    fprintf(stderr, "PASS %d tests\n", ntest);
+    return 0;
+  }
+
+  fprintf(stderr, "FAIL: %d/%d tests failed\n", ntest - ok, ntest);
+
+  return 1;
 }
