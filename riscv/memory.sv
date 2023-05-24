d7f492f6ea45a216070001b8d02e9e72066e0a3b
Nelson Elhage <nelhage@nelhage.com>
2023-05-23 19:33:40 -0700

    memory bugfix and a test

--- riscv/memory.sv
+++ riscv/memory.sv
@@ -3,9 +3,9 @@ module memory(input logic clk, we,
               input logic [31:0]  wa, wd,
               output logic [31:0] rd);
 
-  parameter                       MEMORY_WORDS = 1 << 20;
+  parameter                       MEMORY_WORDS = 1 << 16;
 
-  logic [31:0]                    RAM [(MEMORY_WORDS)>>2-1:0];
+  logic [31:0]                    RAM [(MEMORY_WORDS)-1:0];
 
   /* verilator lint_off WIDTH */
   always_comb
@@ -17,4 +17,10 @@ module memory(input logic clk, we,
   always_ff @(posedge(clk))
     if (we && wa[31:2] < MEMORY_WORDS)
       RAM[wa[31:2]] = wd;
+
+  task initialize_memory(input string filename);
+    $readmemh(filename, RAM);
+  endtask
+
+  export "DPI-C" task initialize_memory;
 endmodule
--- riscv/test/memory_test.cc
+++ riscv/test/memory_test.cc
@@ -1,9 +1,19 @@
 #include <gtest/gtest.h>
 
 #include "Vmemory.h"
-#include <memory>
-#include <verilated.h>
 
+#include "svdpi.h"
+#include <verilated.h>
+#include <verilated_syms.h>
+
+#include <fmt/core.h>
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using std::ios;
 using std::make_unique;
 
 TEST(RISCVTest, MemoryTest) {
@@ -11,24 +21,65 @@ TEST(RISCVTest, MemoryTest) {
 
   Vmemory memory;
 
+  auto scope = svGetScopeFromName("TOP.memory");
+  ASSERT_NE(scope, nullptr);
+
+  std::vector<uint32_t> mem_fill;
+  mem_fill.resize(1 << 16);
+  for (auto i = 0; i < mem_fill.size(); i++) {
+    mem_fill[i] =
+        static_cast<uint32_t>((uint64_t(0xbad4d0eecafecafe) * i) >> 32);
+  }
+  {
+    std::ofstream out("testmem.dat", ios::out | ios::trunc);
+    for (auto it = mem_fill.begin(); it != mem_fill.end(); ++it) {
+      out << fmt::format("{:08x}\n", *it);
+    }
+  }
+
+  svSetScope(scope);
+  memory.initialize_memory("testmem.dat");
+
   memory.clk = 0;
   memory.eval();
 
-  memory.we = 1;
-  memory.wa = 1;
-  memory.wd = 0xdeadbeef;
-  memory.clk = 1;
-  memory.eval();
-
-  memory.clk = 0;
-  memory.eval();
-
+  // combinatorial read
   memory.we = 0;
-  memory.ra = 1;
+  for (auto i = 0; i < mem_fill.size(); i += 129) {
+    memory.ra = i << 2;
+    memory.eval();
+    EXPECT_EQ(memory.rd, mem_fill[i]) << fmt::format("addr={}", i);
+  }
+
+  // writes
+  memory.wa = 512;
+  memory.wd = 0xabcd4321;
+  memory.eval();
+  memory.we = 1;
   memory.clk = 1;
   memory.eval();
 
-  EXPECT_EQ(memory.rd, 0xdeadbeef);
+  memory.ra = 512;
+  memory.eval();
+  EXPECT_EQ(memory.rd, 0xabcd4321);
+
+  memory.clk = 0;
+  memory.wa = 512;
+  memory.wd = 0xd00d;
+  memory.we = 0;
+  memory.eval();
+  memory.clk = 1;
+  memory.eval();
+
+  EXPECT_EQ(memory.rd, 0xabcd4321);
+
+  memory.ra = (mem_fill.size()) << 2;
+  memory.eval();
+  EXPECT_EQ(memory.rd, 0);
+
+  memory.ra = (mem_fill.size() - 1) << 2;
+  memory.eval();
+  EXPECT_EQ(memory.rd, mem_fill.back());
 
   memory.final();
 }
