593dd12777d93b9f3cddb6dff22d36773c8f82be
Nelson Elhage <nelhage@nelhage.com>
2023-05-24 18:52:36 -0700

    regfile reset

--- riscv/regfile.sv
+++ riscv/regfile.sv
@@ -1,13 +1,18 @@
-module regfile(input  logic        clk,
-               input  logic        we3,
-               input  logic [4:0]  ra1, ra2, wa3,
-               input  logic [31:0] wd3,
+module regfile(input logic         clk, reset,
+               input logic         we3,
+               input logic [4:0]   ra1, ra2, wa3,
+               input logic [31:0]  wd3,
                output logic [31:0] rd1, rd2);
 
   logic [31:0] rf[31:0];
 
   always_ff @(posedge clk)
-    if (we3) rf[wa3] <= wd3;
+    begin
+      if (reset)
+        rf <= '{default:0};
+      else if (we3)
+        rf[wa3] <= wd3;
+    end
 
   assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
   assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
